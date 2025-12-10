6910cfdd53d686ad944cc641bc880775bd334d31
Yee <yee.devindie@gmail.com>
2025-12-10 11:51:02 -0600

    L26 - Setting Up Fade to Black When Respawning

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Image blackScreen;
+    public float fadeSpeed = 2f;
+    public bool fadeToBlack, fadeFromBlack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
