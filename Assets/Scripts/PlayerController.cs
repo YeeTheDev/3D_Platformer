7d83a6830d595f1860ef9e3efb52a5e3e85a182b
Yee <yee.devindie@gmail.com>
2025-12-09 18:36:28 -0600

    L13 - Facing the right Direction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,6 +14,9 @@ public class PlayerController : MonoBehaviour
 
     private Camera theCam;
 
+    public GameObject playerModel;
+    public float rotateSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -43,6 +46,10 @@ public class PlayerController : MonoBehaviour
         if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
         {
             transform.rotation = Quaternion.Euler(0f, theCam.transform.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0, moveDirection.z));
+            //playerModel.transform.rotation = newRotation;
+
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
         }
     }
 }
